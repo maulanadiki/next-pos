32a7de20d9d76cf567ddfa334fcbae4f593dc355
maulanadiki <maulanadiki23@gmail.com>
2023-07-03 18:40:47 +0700

    perbaikan cards

---
+++ src/app/home/DataCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export const DataCard = [
+  {
+    id:1,
+    judul:'Member',
+    jenis : 3,
+    jumlah: 10,
+    pendapatan:200000
+  },
+  {
+    id:2,
+    judul:'Non-Member',
+    jenis: 4,
+    jumlah: 6,
+    pendapatan:150000
+  },
+  {
+    id:3,
+    judul:'Redem Vocher',
+    jenis_redem : 5,
+    jumlah_redem: 50000
+  },
+  {
+    id:4,
+    judul:'Redem Vocher',
+    jenis_redem : 10,
+    jumlah_redem: 100000
+  },
+]
--- src/app/home/card.jsx
+++ src/app/home/card.jsx
@@ -1,39 +1,57 @@
 import React from 'react'
+import { DataCard } from './DataCard'
+import './card.css'
 
 const Card = () => {
-    return (
-        <div className='card__container'>
-            <div className="content__card-container">
-                <div className="card w-96 shadow-xl card1">
-                    <div className="card-body">
-                        <h2 className="card-title">Member</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn card3">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card w-96 shadow-xl card2">
-                    <div className="card-body">
-                        <h2 className="card-title">Non Member</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn card3">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card w-96 shadow-xl card3">
-                    <div className="card-body">
-                        <h2 className="card-title">Total Pencucian</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn card3">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+  const renderData = (data) => {
+    switch (data.jenis || data.jenis_redem) {
+      case 3:
+        return (
+          <div key={data.id} className='content__card-list'>
+            <h3>{data.judul}</h3>
+            <p>Jenis Kendaraan : {data.jenis}</p>
+            <p>Jumlah: {data.jumlah}</p>
+            <p>Rp. {data.pendapatan}</p>
+          </div>
+        );
+      case 4:
+        return (
+          <div key={data.id} className='content__card-list'>
+            <h3>{data.judul}</h3>
+            <p>Jenis Kendaraan : {data.jenis}</p>
+            <p>Jumlah: {data.jumlah}</p>
+            <p> Rp. {data.pendapatan}</p>
+          </div>
+        );
+      case 5:
+        return (
+          <div key={data.id} className='content__card-list'>
+            <h3>{data.judul}</h3>
+            <p>Jumlah Redem: {data.jenis_redem} vocher</p>
+            <p>Rp. {data.jumlah_redem}</p>
+          </div>
+        );
+      case 6:
+        return (
+          <div key={data.id} className='content__card-list'>
+            <h3>{data.judul}</h3>
+            <p>Jumlah Redem: {data.jenis_redem} vocher</p>
+            <p>Rp. {data.jumlah_redem}</p>
+          </div>
+        );
+
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className='card__container'>
+      <div className="content__card-container">
+        {DataCard.map((data) => renderData(data))}
+      </div>
+    </div>
+  )
 }
 
 export default Card
